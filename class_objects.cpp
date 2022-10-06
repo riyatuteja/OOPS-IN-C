a357cc1b9cb84885f2b54f2d6c2a1ad29d52fd41
Riya <68150559+riyatuteja@users.noreply.github.com>
2022-10-06 20:47:51 +0530

    Update class_objects.cpp

--- class_objects.cpp
+++ class_objects.cpp
@@ -26,6 +26,20 @@ int main()
     cin>>p1.name>>p1.age>>p1.gender;
 
     p1.getDetails();
+    
+    // for more thn one object i can use arrays
+    
+     person arr[4];
+    for(int i=0;i<4;i++)
+    {
+        cin>>arr[i].name>>arr[i].age>>arr[i].gender;
+
+    }
+    for(int i=0;i<4;i++)
+    {
+        arr[i].getDetails();
+
+    }
 
     return 0;
 
